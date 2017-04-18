b920301a73c66897ea1de50cbc12236aad2e3ac5
biljana lukovic <biljana.lukovic@securekey.com>
2017-04-18 13:39:02 -0400

    [FAB-3324] Get organization units
    Change-Id: I9c9dab93e3d71b47fd4a860447bf521d4f4d025f
    Signed-off-by: biljana lukovic <biljana.lukovic@securekey.com>

--- fabric-client/chain.go
+++ fabric-client/chain.go
@@ -88,7 +88,7 @@ type Chain interface {
 	SendTransaction(tx *Transaction) ([]*TransactionResponse, error)
 	SendInstallProposal(chaincodeName string, chaincodePath string, chaincodeVersion string, chaincodePackage []byte, targets []Peer) ([]*TransactionProposalResponse, string, error)
 	SendInstantiateProposal(chaincodeName string, chainID string, args []string, chaincodePath string, chaincodeVersion string, targets []Peer) ([]*TransactionProposalResponse, string, error)
-
+	GetOrganizationUnits() ([]string, error)
 	QueryExtensionInterface() ChainExtension
 }
 
@@ -422,6 +422,25 @@ func (c *chain) SetMSPManager(mspManager msp.MSPManager) {
 // GetMSPManager returns the MSP Manager for this channel
 func (c *chain) GetMSPManager() msp.MSPManager {
 	return c.mspManager
+}
+
+// GetOrganizationUnits - to get identifier for the organization configured on the channel
+func (c *chain) GetOrganizationUnits() ([]string, error) {
+	chainMSPManager := c.GetMSPManager()
+	msps, err := chainMSPManager.GetMSPs()
+	if err != nil {
+		logger.Info("Cannot get channel manager")
+		return nil, fmt.Errorf("Organization uits were not set: %v", err)
+	}
+	var orgIdentifiers []string
+	for _, v := range msps {
+		orgName, err := v.GetIdentifier()
+		if err != nil {
+			logger.Info("Organization does not have an identifier")
+		}
+		orgIdentifiers = append(orgIdentifiers, orgName)
+	}
+	return orgIdentifiers, nil
 }
 
 // Initialize initializes the chain
