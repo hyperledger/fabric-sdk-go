78b6da1c1604cf90f7f09415cd49bdb63e3648ee
Divyank Katira <Divyank.Katira@securekey.com>
2017-05-03 17:27:19 -0400

    Return correct exit code from test script
    Change-Id: Ib0d5f0374b5bcf72aef3a23e467d25503259463e
    Signed-off-by: Divyank Katira <Divyank.Katira@securekey.com>

--- test/scripts/integration.sh
+++ test/scripts/integration.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -exu
 
 # Packages to exclude
 PKGS=`go list github.com/hyperledger/fabric-sdk-go/test/... 2> /dev/null | \
@@ -11,5 +11,13 @@ echo "Running integration tests..."
 cd ../../
 gocov test -ldflags "$GO_LDFLAGS" $PKGS -p 1 -timeout=10m | gocov-xml > integration-report.xml
 
-echo "Cleaning up..."
-cd ./test/fixtures && docker-compose down
+if [ $? -eq 0 ]
+then
+  echo "Integration tests passed. Cleaning up..."
+  cd ./test/fixtures && docker-compose down
+  exit 0
+else
+  echo "Integration tests failed. Cleaning up..."
+  cd ./test/fixtures && docker-compose down
+  exit 1
+fi
