9a8e8f5f2de5bb3f3495c4f50f5edd27adaa3411
Sudesh Shetty <sudesh.shetty@securekey.com>
2018-02-12 11:58:01 -0500

    [FAB-8189] no orderer config test refactoring
    Change-Id: I58b11ba1c4e6ee59bc54b67600253ae6f132f0fc
    Signed-off-by: Sudesh Shetty <sudesh.shetty@securekey.com>

--- test/integration/e2e/end_to_end.go
+++ test/integration/e2e/end_to_end.go
@@ -14,8 +14,6 @@ import (
 
 	"github.com/hyperledger/fabric-sdk-go/api/apiconfig"
 
-	"github.com/hyperledger/fabric-sdk-go/api/apifabclient"
-
 	"github.com/hyperledger/fabric-sdk-go/test/integration"
 	"github.com/hyperledger/fabric-sdk-go/test/metadata"
 	"github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/common/cauthdsl"
@@ -38,10 +36,6 @@ const (
 
 func runWithConfigFixture(t *testing.T) {
 	Run(t, config.FromFile("../"+integration.ConfigTestFile))
-}
-
-func runWithNoOrdererConfigFixture(t *testing.T) {
-	RunWithNoOrdererConfig(t, config.FromFile("../"+integration.ConfigChBlockTestFile))
 }
 
 // Run enables testing an end-to-end scenario against the supplied SDK options
@@ -162,84 +156,4 @@ func Run(t *testing.T, configOpt apiconfig.ConfigProvider, sdkOpts ...fabsdk.Opt
 		t.Fatalf("Execute failed. Before: %s, after: %s", value, response.Payload)
 	}
 
-}
-
-type mockDiscoveryFilter struct {
-	called bool
-}
-
-// Accept returns true if this peer is to be included in the target list
-func (df *mockDiscoveryFilter) Accept(peer apifabclient.Peer) bool {
-	df.called = true
-	return true
-}
-
-// RunWithNoOrdererConfig enables chclient scenarios using config and sdk options provided
-func RunWithNoOrdererConfig(t *testing.T, configOpt apiconfig.ConfigProvider, sdkOpts ...fabsdk.Option) {
-
-	sdk, err := fabsdk.New(configOpt, sdkOpts...)
-	if err != nil {
-		t.Fatalf("Failed to create new SDK: %s", err)
-	}
-
-	// ************ Test setup complete ************** //
-
-	//Discovery filter added to test discovery filter behavior
-	discoveryFilter := &mockDiscoveryFilter{called: false}
-	// Channel client is used to query and execute transactions (Org1 is default org)
-	chClient, err := sdk.NewClient(fabsdk.WithUser("User1")).Channel(channelID, fabsdk.WithTargetFilter(discoveryFilter))
-	if err != nil {
-		t.Fatalf("Failed to create new channel client: %s", err)
-	}
-
-	// Release all channel client resources
-	defer chClient.Close()
-
-	response, err := chClient.Query(chclient.Request{ChaincodeID: ccID, Fcn: "invoke", Args: integration.ExampleCCQueryArgs()})
-	if err != nil {
-		t.Fatalf("Failed to query funds: %s", err)
-	}
-	value := response.Payload
-
-	//Test if discovery filter is being called
-	if !discoveryFilter.called {
-		t.Fatalf("discoveryFilter not called")
-	}
-
-	eventID := "test([a-zA-Z]+)"
-
-	// Register chaincode event (pass in channel which receives event details when the event is complete)
-	notifier := make(chan *chclient.CCEvent)
-	rce := chClient.RegisterChaincodeEvent(notifier, ccID, eventID)
-
-	// Move funds
-	response, err = chClient.Execute(chclient.Request{ChaincodeID: ccID, Fcn: "invoke", Args: integration.ExampleCCTxArgs()})
-	if err != nil {
-		t.Fatalf("Failed to move funds: %s", err)
-	}
-
-	select {
-	case ccEvent := <-notifier:
-		t.Logf("Received CC event: %s\n", ccEvent)
-	case <-time.After(time.Second * 20):
-		t.Fatalf("Did NOT receive CC event for eventId(%s)\n", eventID)
-	}
-
-	// Unregister chain code event using registration handle
-	err = chClient.UnregisterChaincodeEvent(rce)
-	if err != nil {
-		t.Fatalf("Unregister cc event failed: %s", err)
-	}
-
-	// Verify move funds transaction result
-	response, err = chClient.Query(chclient.Request{ChaincodeID: ccID, Fcn: "invoke", Args: integration.ExampleCCQueryArgs()})
-	if err != nil {
-		t.Fatalf("Failed to query funds after transaction: %s", err)
-	}
-
-	valueInt, _ := strconv.Atoi(string(value))
-	valueAfterInvokeInt, _ := strconv.Atoi(string(response.Payload))
-	if valueInt+1 != valueAfterInvokeInt {
-		t.Fatalf("Execute failed. Before: %s, after: %s", value, response.Payload)
-	}
 }
---
+++ test/integration/e2e/no_orderer_config.go
@@ -0,0 +1,104 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package e2e
+
+import (
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/hyperledger/fabric-sdk-go/api/apiconfig"
+	"github.com/hyperledger/fabric-sdk-go/api/apifabclient"
+	"github.com/hyperledger/fabric-sdk-go/api/apitxn/chclient"
+	"github.com/hyperledger/fabric-sdk-go/pkg/config"
+	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
+	"github.com/hyperledger/fabric-sdk-go/test/integration"
+)
+
+func runWithNoOrdererConfigFixture(t *testing.T) {
+	runWithNoOrdererConfig(t, config.FromFile("../"+integration.ConfigChBlockTestFile))
+}
+
+// RunWithNoOrdererConfig enables chclient scenarios using config and sdk options provided
+func runWithNoOrdererConfig(t *testing.T, configOpt apiconfig.ConfigProvider, sdkOpts ...fabsdk.Option) {
+
+	sdk, err := fabsdk.New(configOpt, sdkOpts...)
+	if err != nil {
+		t.Fatalf("Failed to create new SDK: %s", err)
+	}
+
+	// ************ Test setup complete ************** //
+
+	//Discovery filter added to test discovery filter behavior
+	discoveryFilter := &mockDiscoveryFilter{called: false}
+	// Channel client is used to query and execute transactions (Org1 is default org)
+	chClient, err := sdk.NewClient(fabsdk.WithUser("User1")).Channel(channelID, fabsdk.WithTargetFilter(discoveryFilter))
+	if err != nil {
+		t.Fatalf("Failed to create new channel client: %s", err)
+	}
+
+	// Release all channel client resources
+	defer chClient.Close()
+
+	response, err := chClient.Query(chclient.Request{ChaincodeID: ccID, Fcn: "invoke", Args: integration.ExampleCCQueryArgs()})
+	if err != nil {
+		t.Fatalf("Failed to query funds: %s", err)
+	}
+	value := response.Payload
+
+	//Test if discovery filter is being called
+	if !discoveryFilter.called {
+		t.Fatalf("discoveryFilter not called")
+	}
+
+	eventID := "test([a-zA-Z]+)"
+
+	// Register chaincode event (pass in channel which receives event details when the event is complete)
+	notifier := make(chan *chclient.CCEvent)
+	rce := chClient.RegisterChaincodeEvent(notifier, ccID, eventID)
+
+	// Move funds
+	response, err = chClient.Execute(chclient.Request{ChaincodeID: ccID, Fcn: "invoke", Args: integration.ExampleCCTxArgs()})
+	if err != nil {
+		t.Fatalf("Failed to move funds: %s", err)
+	}
+
+	select {
+	case ccEvent := <-notifier:
+		t.Logf("Received CC event: %s\n", ccEvent)
+	case <-time.After(time.Second * 20):
+		t.Fatalf("Did NOT receive CC event for eventId(%s)\n", eventID)
+	}
+
+	// Unregister chain code event using registration handle
+	err = chClient.UnregisterChaincodeEvent(rce)
+	if err != nil {
+		t.Fatalf("Unregister cc event failed: %s", err)
+	}
+
+	// Verify move funds transaction result
+	response, err = chClient.Query(chclient.Request{ChaincodeID: ccID, Fcn: "invoke", Args: integration.ExampleCCQueryArgs()})
+	if err != nil {
+		t.Fatalf("Failed to query funds after transaction: %s", err)
+	}
+
+	valueInt, _ := strconv.Atoi(string(value))
+	valueAfterInvokeInt, _ := strconv.Atoi(string(response.Payload))
+	if valueInt+1 != valueAfterInvokeInt {
+		t.Fatalf("Execute failed. Before: %s, after: %s", value, response.Payload)
+	}
+}
+
+type mockDiscoveryFilter struct {
+	called bool
+}
+
+// Accept returns true if this peer is to be included in the target list
+func (df *mockDiscoveryFilter) Accept(peer apifabclient.Peer) bool {
+	df.called = true
+	return true
+}
