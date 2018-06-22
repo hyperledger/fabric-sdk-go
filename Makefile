ed6420081d5d73bef6f88a87718b93e048b4b8e8
Troy Ronda <troy@troyronda.com>
2018-06-22 12:43:17 -0400

    [FAB-10838] CI lints only changed packages
    This change updates the linter script to detect modified
    packages, and only run linting on those packages.
    
    A new Make target lint-all has been added to allow for
    linting all packages.
    
    Change-Id: I4b0ce6f2cc06c7b72ccffd96b8e35a27d3821a7f
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- Makefile
+++ Makefile
@@ -176,7 +176,7 @@ export DOCKER_CMD
 export DOCKER_COMPOSE_CMD
 
 .PHONY: all
-all: checks unit-test integration-test
+all: checks lint-all unit-test integration-test
 
 .PHONY: depend
 depend:
@@ -195,8 +195,11 @@ license:
 
 .PHONY: lint
 lint: populate
-	@$(TEST_SCRIPTS_PATH)/check_lint.sh
+	@LINT_CHANGED_ONLY=true $(TEST_SCRIPTS_PATH)/check_lint.sh
 
+.PHONY: lint-all
+lint-all: populate
+	@$(TEST_SCRIPTS_PATH)/check_lint.sh
 
 .PHONY: build-softhsm2-image
 build-softhsm2-image:
--- test/scripts/check_lint.sh
+++ test/scripts/check_lint.sh
@@ -7,23 +7,98 @@
 # This script runs Go linting and vetting tools
 
 set -e
-
-
-
+LINT_CHANGED_ONLY="${LINT_CHANGED_ONLY:-false}"
+GO_CMD="${GO_CMD:-go}"
+REPO="github.com/hyperledger/fabric-sdk-go"
 GOMETALINT_CMD=gometalinter
 
+function findPackages {
+    # Find all packages that should be linted.
+    declare -a src=(
+    "./pkg"
+    "./test"
+    )
 
-PROJECT_PATH=$GOPATH/src/github.com/hyperledger/fabric-sdk-go
+    PKGS=()
+    for i in "${src[@]}"
+    do
+       PKG_LIST=`$GO_CMD list $i/... 2> /dev/null`
+       while read -r line; do
+          PKGS+=("$line")
+       done <<< "$PKG_LIST"
+    done
+}
 
-declare -a arr=(
-"./pkg"
-"./test"
-)
+function findChangedPackages {
+    # Determine which directories have changes.
+    CHANGED=$(git diff --name-only --diff-filter=ACMRTUXB HEAD)
 
+    if [[ "$CHANGED" != "" ]]; then
+        CHANGED+=$'\n'
+    fi
+
+    LAST_COMMITS=($(git log -2 --pretty=format:"%h"))
+    CHANGED+=$(git diff-tree --no-commit-id --name-only --diff-filter=ACMRTUXB -r ${LAST_COMMITS[1]} ${LAST_COMMITS[0]})
+
+    CHANGED_PKGS=()
+    while read -r line; do
+        if [ "$line" != "" ]; then
+            DIR=`dirname $line`
+            if [ "$DIR" = "." ]; then
+                CHANGED_PKGS+=("$REPO")
+            else
+                CHANGED_PKGS+=("$REPO/$DIR")
+            fi
+        fi
+    done <<< "$CHANGED"
+    CHANGED_PKGS=($(printf "%s\n" "${CHANGED_PKGS[@]}" | sort -u | tr '\n' ' '))
+}
+
+function filterExcludedPackages {
+    FILTERED_PKGS=()
+
+    for pkg in "${PKGS[@]}"
+    do
+        for i in "${CHANGED_PKGS[@]}"
+        do
+            if [ "$pkg" = "$i" ]; then
+              FILTERED_PKGS+=("$pkg")
+            fi
+        done
+    done
+
+    PKGS=("${FILTERED_PKGS[@]}")
+}
+
+# packagesToDirs convert packages to directories
+function packagesToDirs {
+    DIRS=()
+    for i in "${PKGS[@]}"
+    do
+        lintdir=${i#$REPO/}
+        DIRS+=($lintdir)
+    done
+}
+
+findPackages
+
+# Reduce Linter checks to changed packages.
+if [ "$LINT_CHANGED_ONLY" = true ]; then
+    findChangedPackages
+    filterExcludedPackages
+fi
+
+packagesToDirs
+
+if [ ${#DIRS[@]} -eq 0 ]; then
+    echo "Skipping linter since no packages were changed"
+    exit 0
+fi
+
+if [ "$LINT_CHANGED_ONLY" = true ]; then
+    echo "Changed directories to lint: ${DIRS[@]}"
+fi
 
 echo "Running metalinters..."
-for i in "${arr[@]}"
-do
-   echo "Checking $i"
-   $GOMETALINT_CMD --config=./gometalinter.json $i/...
-done
+$GOMETALINT_CMD --config=./gometalinter.json "${DIRS[@]}"
+echo "Metalinters finished successfully"
