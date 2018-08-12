28ed3d224f4f549949135c360d86d58bdd171a69
ping40 <norberthu30@gmail.com>
2018-08-12 12:13:29 +0800

    [FABG-714] fix typoes
    Change-Id: Id982afa80dc6bd3f74ca81b5dc9c13677ee99c7e
    Signed-off-by: ping40 <norberthu30@gmail.com>

--- pkg/core/logging/metadata/callerInfo_test.go
+++ pkg/core/logging/metadata/callerInfo_test.go
@@ -9,14 +9,22 @@ package metadata
 import (
 	"testing"
 
-	"github.com/hyperledger/fabric-sdk-go/pkg/core/logging/api"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/hyperledger/fabric-sdk-go/pkg/core/logging/api"
 )
 
 func TestCallerInfoSetting(t *testing.T) {
 
 	sampleCallerInfoSetting := CallerInfo{}
 	samppleModuleName := "sample-module-name"
+
+	//By default caller info should be enabled if not set
+	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleModuleName, api.DEBUG), "Callerinfo supposed to be enabled for this level")
+	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleModuleName, api.INFO), "Callerinfo supposed to be enabled for this level")
+	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleModuleName, api.WARNING), "Callerinfo supposed to be enabled for this level")
+	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleModuleName, api.ERROR), "Callerinfo supposed to be enabled for this level")
+	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleModuleName, api.CRITICAL), "Callerinfo supposed to be enabled for this level")
 
 	sampleCallerInfoSetting.ShowCallerInfo(samppleModuleName, api.DEBUG)
 	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleModuleName, api.DEBUG), "Callerinfo supposed to be enabled for this level")
@@ -27,18 +35,11 @@ func TestCallerInfoSetting(t *testing.T) {
 	//Reset existing caller info setting
 	sampleCallerInfoSetting.showcaller = nil
 
-	//By default caller info should be disabled if not set
-	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleModuleName, api.DEBUG), "Callerinfo supposed to be enabled for this level")
-	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleModuleName, api.INFO), "Callerinfo supposed to be disabled for this level")
-	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleModuleName, api.WARNING), "Callerinfo supposed to be disabled for this level")
-	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleModuleName, api.ERROR), "Callerinfo supposed to be disabled for this level")
-	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleModuleName, api.CRITICAL), "Callerinfo supposed to be disabled for this level")
-
-	//By default caller info should be disabled if module name not found
+	//By default caller info should be enabled for any module name
 	samppleInvalidModuleName := "sample-module-name-doesnt-exists"
-	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleInvalidModuleName, api.INFO), "Callerinfo supposed to be disabled for this level")
-	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleInvalidModuleName, api.WARNING), "Callerinfo supposed to be disabled for this level")
-	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleInvalidModuleName, api.ERROR), "Callerinfo supposed to be disabled for this level")
-	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleInvalidModuleName, api.CRITICAL), "Callerinfo supposed to be disabled for this level")
-	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleInvalidModuleName, api.DEBUG), "Callerinfo supposed to be disabled for this level")
+	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleInvalidModuleName, api.INFO), "Callerinfo supposed to be enabled for this level")
+	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleInvalidModuleName, api.WARNING), "Callerinfo supposed to be enabled for this level")
+	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleInvalidModuleName, api.ERROR), "Callerinfo supposed to be enabled for this level")
+	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleInvalidModuleName, api.CRITICAL), "Callerinfo supposed to be enabled for this level")
+	assert.True(t, sampleCallerInfoSetting.IsCallerInfoEnabled(samppleInvalidModuleName, api.DEBUG), "Callerinfo supposed to be enabled for this level")
 }
