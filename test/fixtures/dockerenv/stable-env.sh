427c3294a38c2ff1be2fa1c39a4b31b6917b3cd1
Troy Ronda <troy@troyronda.com>
2018-08-23 21:15:14 -0400

    [FABG-739] Use 1.2.1 images from dev registry
    This change temporarily uses the 1.2.1 images from
    the dev registry to stablize tests.
    
    Change-Id: If40d3daa9999363b83ecf2d7a8e199b4ff736968
    Signed-off-by: Troy Ronda <troy@troyronda.com>

---
+++ test/fixtures/dockerenv/stable-env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright SecureKey Technologies Inc. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# This file contains environment overrides to enable testing against
+# the stable target
+
+export FABRIC_CA_FIXTURE_TAG="1.2.1-stable"
+export FABRIC_ORDERER_FIXTURE_TAG="1.2.1-stable"
+export FABRIC_PEER_FIXTURE_TAG="1.2.1-stable"
+export FABRIC_COUCHDB_FIXTURE_TAG="1.2.1-stable"
+export FABRIC_BUILDER_FIXTURE_TAG="1.2.1-stable"
