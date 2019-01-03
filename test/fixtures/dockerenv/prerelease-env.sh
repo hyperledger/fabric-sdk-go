f53e21bdaaf87585a0def56834bdeb925ab94721
Troy Ronda <troy@troyronda.com>
2019-01-03 10:49:41 -0500

    [FABG-804] Update prerelease target to 1.4 RC2
    Change-Id: Ia7875ffc196e4c9121c1be51626a2ae92075d215
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- test/fixtures/dockerenv/prerelease-env.sh
+++ test/fixtures/dockerenv/prerelease-env.sh
@@ -7,16 +7,10 @@
 
 # This file contains environment overrides to enable testing
 # against the latest pre-release target.
-export FABRIC_FIXTURE_VERSION="v1.3"
+export FABRIC_FIXTURE_VERSION="v1.4"
 export FABRIC_CRYPTOCONFIG_VERSION="v1"
 
-export FABRIC_CA_FIXTURE_TAG="1.3.0-rc1"
-export FABRIC_ORDERER_FIXTURE_TAG="1.3.0-rc1"
-export FABRIC_PEER_FIXTURE_TAG="1.3.0-rc1"
-export FABRIC_BUILDER_FIXTURE_TAG="1.3.0-rc1"
-
-# override SDK configuration that loads crypto-config
-export FABRIC_SDK_CLIENT_CRYPTOCONFIG_PATH='${GOPATH}'"/src/github.com/hyperledger/fabric-sdk-go/test/fixtures/fabric/${FABRIC_CRYPTOCONFIG_VERSION}/crypto-config"
-export FABRIC_SDK_CLIENT_ORDERERS_TLSCACERTS_PATH='${GOPATH}'"/src/github.com/hyperledger/fabric-sdk-go/test/fixtures/fabric/${FABRIC_CRYPTOCONFIG_VERSION}/crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem"
-export FABRIC_SDK_CLIENT_PEERS_PEER0_ORG1_EXAMPLE_COM_TLSCACERTS_PATH='${GOPATH}'"/src/github.com/hyperledger/fabric-sdk-go/test/fixtures/fabric/${FABRIC_CRYPTOCONFIG_VERSION}/crypto-config/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem"
-export FABRIC_SDK_CLIENT_PEERS_PEER0_ORG2_EXAMPLE_COM_TLSCACERTS_PATH='${GOPATH}'"/src/github.com/hyperledger/fabric-sdk-go/test/fixtures/fabric/${FABRIC_CRYPTOCONFIG_VERSION}/crypto-config/peerOrganizations/org2.example.com/tlsca/tlsca.org2.example.com-cert.pem"
+export FABRIC_CA_FIXTURE_TAG="1.4.0-rc2"
+export FABRIC_ORDERER_FIXTURE_TAG="1.4.0-rc2"
+export FABRIC_PEER_FIXTURE_TAG="1.4.0-rc2"
+export FABRIC_BUILDER_FIXTURE_TAG="1.4.0-rc2"
