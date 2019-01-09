295d4cbe80811e2d2672f832aae32e60900487f6
Troy Ronda <troy@troyronda.com>
2019-01-09 14:55:39 -0500

    [FABG-811] Update to Fabric 1.4.0
    This change updates the test target for stable to 1.4.0.
    Prev is updated to 1.3.0.
    
    Change-Id: I07282c6a44c32fa6c565ac7bfa4ae395e40fd3bb
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- test/fixtures/dockerenv/prev-env.sh
+++ test/fixtures/dockerenv/prev-env.sh
@@ -8,14 +8,10 @@
 # This file contains environment overrides to enable testing
 # against the latest pre-release target.
 
-export FABRIC_FIXTURE_VERSION="v1.2"
+export FABRIC_FIXTURE_VERSION="v1.3"
 export FABRIC_CRYPTOCONFIG_VERSION="v1"
 
-export FABRIC_CA_FIXTURE_TAG="1.2.1"
-export FABRIC_ORDERER_FIXTURE_TAG="1.2.1"
-export FABRIC_PEER_FIXTURE_TAG="1.2.1"
-export FABRIC_BUILDER_FIXTURE_TAG="1.2.1"
-
-# override configuration that loads crypto-config
-export FABRIC_SDK_CLIENT_CRYPTOCONFIG_PATH='${GOPATH}'"/src/github.com/hyperledger/fabric-sdk-go/test/fixtures/fabric/${FABRIC_CRYPTOCONFIG_VERSION}/crypto-config"
-export FABRIC_SDK_CLIENT_ORDERERS_TLSCACERTS_PATH='${GOPATH}'"/src/github.com/hyperledger/fabric-sdk-go/test/fixtures/fabric/${FABRIC_CRYPTOCONFIG_VERSION}/crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem"
+export FABRIC_CA_FIXTURE_TAG="1.3.0"
+export FABRIC_ORDERER_FIXTURE_TAG="1.3.0"
+export FABRIC_PEER_FIXTURE_TAG="1.3.0"
+export FABRIC_BUILDER_FIXTURE_TAG="1.3.0"
