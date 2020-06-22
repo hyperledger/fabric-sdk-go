67ba62c04d3755bb22dc0579285ebabf99f8c431
Egof <egor.pol9kov@gmail.com>
2020-06-23 01:12:29 +0300

    FABG-968: fix nil pointer

--- pkg/client/channel/invoke/txnhandler.go
+++ pkg/client/channel/invoke/txnhandler.go
@@ -60,7 +60,8 @@ func (e *EndorsementHandler) Handle(requestContext *RequestContext, clientContex
 	if err != nil {
 		if len(transactionProposalResponses) > 0 &&
 			transactionProposalResponses[0] != nil &&
-			transactionProposalResponses[0].Response != nil {
+			transactionProposalResponses[0].Response != nil &&
+			requestContext != nil {
 			requestContext.Response.Payload = transactionProposalResponses[0].Response.Payload
 		}
 		requestContext.Error = err
