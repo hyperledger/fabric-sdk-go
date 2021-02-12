1404312d0c3ae5173758d7180a7630d317535ac4
Vadim Inshakov <vadiminshakov@gmail.com>
2021-02-12 17:13:27 +0500

    add copyright header
    Signed-off-by: Vadim Inshakov <vadiminshakov@gmail.com>

--- internal/github.com/hyperledger/fabric/bccsp/sw/vault.go
+++ internal/github.com/hyperledger/fabric/bccsp/sw/vault.go
@@ -1,3 +1,13 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+/*
+Notice: This file has been modified for Hyperledger Fabric SDK Go usage.
+Please review third_party pinning scripts and patches for more details.
+*/
+
 package sw
 
 import (
