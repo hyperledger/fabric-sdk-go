483d9e1a3e684ea3ed4b890820e158c97c94b09e
Vladyslav Kopaihorodskyi <vlad.kopaygorodsky@gmail.com>
2021-05-28 00:21:39 +0300

    Fixed a bug in proposal matcher when querying committed chaincodes. P… (#177)
    * Fixed a bug in proposal matcher when querying committed chaincodes. Peers return cc definitions in unexpected order. proto.Equal fails since slice struct is used and order is important.
    
    Signed-off-by: Vladyslav Kopaihorodskyi <vlad.kopaygorodsky@gmail.com>
    
    * Renamed unmarshalCCDefResults. Added test to verify that UnmarshalInOrderCCDefResults works correctly and sorts chaincodes by name
    
    Signed-off-by: Vladyslav Kopaihorodskyi <vlad.kopaygorodsky@gmail.com>
    
    * goimported lifecycleprocessor_test.go
    
    Signed-off-by: Vladyslav Kopaihorodskyi <vlad.kopaygorodsky@gmail.com>
    
    * added licence to lifecycleprocessor_test.go
    
    Signed-off-by: Vladyslav Kopaihorodskyi <vlad.kopaygorodsky@gmail.com>
    
    * modified comment
    
    Signed-off-by: Vladyslav Kopaihorodskyi <vlad.kopaygorodsky@gmail.com>
    
    * licence fix in lifecycleprocessor_test.go
    
    Signed-off-by: Vladyslav Kopaihorodskyi <vlad.kopaygorodsky@gmail.com>

--- pkg/client/resmgmt/lifecycleprocessor.go
+++ pkg/client/resmgmt/lifecycleprocessor.go
@@ -9,6 +9,7 @@ package resmgmt
 import (
 	reqContext "context"
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/golang/protobuf/proto"
@@ -281,7 +282,7 @@ func (p *lifecycleProcessor) queryCommitted(reqCtx reqContext.Context, channelID
 
 	err = p.verifyResponsesMatch(txProposalResponse,
 		func(payload []byte) (proto.Message, error) {
-			return unmarshalCCDefResults(req.Name, payload)
+			return unmarshalInOrderCCDefResults(req.Name, payload)
 		},
 	)
 	if err != nil {
@@ -564,12 +565,28 @@ func (p *lifecycleProcessor) verifyResponsesMatch(responses []*fab.TransactionPr
 	return nil
 }
 
-func unmarshalCCDefResults(name string, payload []byte) (proto.Message, error) {
+// unmarshalls payload into QueryChaincodeDefinition(s)Result and sorts chaincodes by name
+func unmarshalInOrderCCDefResults(name string, payload []byte) (proto.Message, error) {
 	if name != "" {
 		result := &lb.QueryChaincodeDefinitionResult{}
-		return result, proto.Unmarshal(payload, result)
+		if err := proto.Unmarshal(payload, result); err != nil {
+			return result, errors.Wrapf(err, "failed to unmarshal payload with name %s into QueryChaincodeDefinitionResult", name)
+		}
+
+		return result, nil
 	}
 
 	result := &lb.QueryChaincodeDefinitionsResult{}
-	return result, proto.Unmarshal(payload, result)
+
+	if err := proto.Unmarshal(payload, result); err != nil {
+		return result, errors.Wrap(err, "failed to unmarshal payload into QueryChaincodeDefinitionsResult")
+	}
+
+	// need to sort cc definitions by name because peers return lists of chaincodes in an unexpected order.
+	// proto.Equal fails to compare them because slice is used, thus it compares each value x[i] == x[j]
+	sort.Slice(result.ChaincodeDefinitions, func(i, j int) bool {
+		return result.ChaincodeDefinitions[i].Name > result.ChaincodeDefinitions[j].Name
+	})
+
+	return result, nil
 }
