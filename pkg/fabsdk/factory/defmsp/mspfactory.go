19a331ae1dcf291217b86f732e985cbd79cef6b2
Gennadiy Muzykantov <g@muzykantov.me>
2022-02-06 19:02:53 +0300

    Add filesystem option to the ProviderFactory

--- pkg/fabsdk/factory/defmsp/mspfactory.go
+++ pkg/fabsdk/factory/defmsp/mspfactory.go
@@ -7,6 +7,8 @@ SPDX-License-Identifier: Apache-2.0
 package defmsp
 
 import (
+	"io/fs"
+
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/core"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/fab"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/msp"
@@ -18,10 +20,25 @@ import (
 
 // ProviderFactory represents the default MSP provider factory.
 type ProviderFactory struct {
+	opts providerFactoryOptions
+}
+
+type ProviderFactoryOption func(*providerFactoryOptions) error
+
+// WithFS creates ProviderFactory with fs.FS read only based storage.
+func WithFS(filesystem fs.FS) ProviderFactoryOption {
+	return func(pfo *providerFactoryOptions) error {
+		pfo.filesystem = filesystem
+		return nil
+	}
+}
+
+type providerFactoryOptions struct {
+	filesystem fs.FS
 }
 
 // NewProviderFactory returns the default MSP provider factory.
-func NewProviderFactory() *ProviderFactory {
+func NewProviderFactory(opts ...ProviderFactoryOption) *ProviderFactory {
 	f := ProviderFactory{}
 	return &f
 }
@@ -35,7 +52,10 @@ func (f *ProviderFactory) CreateUserStore(config msp.IdentityConfig) (msp.UserSt
 		return mspimpl.NewMemoryUserStore(), nil
 	}
 
-	stateStore, err := kvs.New(&kvs.FileKeyValueStoreOptions{Path: stateStorePath})
+	stateStore, err := kvs.New(&kvs.FileKeyValueStoreOptions{
+		Path:       stateStorePath,
+		Filesystem: f.opts.filesystem,
+	})
 	if err != nil {
 		return nil, errors.WithMessage(err, "CreateNewFileKeyValueStore failed")
 	}
